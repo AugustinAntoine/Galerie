1e087cd60e0bc63936d36024eb9a37b5ead6c751
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-11-12 13:41:50 +0100

    correction script vers local

--- Script.js
+++ Script.js
@@ -56,7 +56,7 @@ function afficher(json){
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-	fetch("https://keen-wing-4f2746.netlify.app/images.json")
+	fetch("/GalerieRepos/tableau.json")
 		.then(response => response.json())
 		.then(json => generateUI(json));
 });
