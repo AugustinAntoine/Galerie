10ec84520b8c4e39d9cea2b086a8b53960a1dc7e
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-11-19 15:09:24 +0100

    eeeeeee

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
+// Test
 // console.log("test s'il est interprété OK!");
+// Fin Test
 
-self.addEventListener('message',event=>
-{
-    this.clients.matchAll()
-    .then(clients=>
-    {
-        clients.forEach(
-            client=>client.postMessage('Enchanté, je suis le service worker')
-        );
-    });
-});
+// Test si le service worker reconnait les messages (type d'événement = 'message') envoyés
+// self.addEventListener('message',event=>
+// {
+    // this.clients.matchAll()
+    // .then(clients=>
+    // {
+        // clients.forEach(
+            // client=>client.postMessage('Enchanté, je suis le service worker')
+        // );
+    // });
+// });
 
 // Ecoute de tous les évènements fetch et récupération de chaque URL
 //self.addEventListener("fetch", event => {console.log(event.request.url);});
@@ -17,11 +20,13 @@ self.addEventListener('message',event=>
 // Ecoute de tous les évènements fetch et récupération de l’URL permettant l’obtention de images.jsonpuis reformatage de images.json
 self.addEventListener("fetch", (event) => 
 {
+	// Test
 	console.log("On a trouvé un fetch");
-	const url = event.request.url;
 	console.log("L'url c'est ça : " + url);
 	console.log("L'url.indexOf c'est ça : " + url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json"));
+	// Fin Test
 	
+	const url = event.request.url;
 	if (url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json") ===0) 
 	{
 		console.log("Le fetch est sur une requête vers notre tableau.json");
