11ce67ebf04e4f9b520d620db80f35b28a9bb658
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-11-19 09:49:41 +0100

    ok

--- sw.js
+++ sw.js
@@ -9,8 +9,40 @@ self.addEventListener('message',event=>
     });
 });
 
-// Si on reconnait un fetch, on écrit "PWA!!!!"
-self.addEventListener('fetch',event=>
-{    
-    console.log(newResponse('PWA!!!!'));
+// Ecoute de tous les évènements fetch et récupération de chaque URL
+self.addEventListener("fetch", event => {console.log(event.request.url);});
+
+// Ecoute de tous les évènements fetch et récupération de l’URLpermettant l’obtention de images.jsonpuis reformatage de images.json
+self.addEventListener("fetch", (event) => 
+{
+	const url = event.request.url;
+	if (url.indexOf("https://monNomDeDomaine/images.json") ===0) 
+	{
+		event.respondWith(fetch(event.request)
+		.then((response) => 
+		{
+			if (response.statusText !== "OK") 
+			{
+				console.error("Service Worker","Error when fetching",event.request.url);
+				return response;
+			}
+			console.info("Formatting data");
+			return response.json()
+			.then((json) => 
+			{
+				const formattedResponse = json.map((j) => (
+				{
+					name: j.name,description: j.description || "",updated_at: j.updated_at,
+				})
+				);
+				return new Response(JSON.stringify(formattedResponse));
+			});
+		}));
+	}
 });
+
+// Si on reconnait un fetch, on écrit "PWA!!!!"
+// self.addEventListener('fetch',event=>
+// {    
+    // console.log(newResponse('PWA!!!!'));
+// });
