8de3ce7bb5ce2dd2631b40d056b4be8224636183
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-11-26 13:09:15 +0100

    correction ex6 car ma solution ne pouvait marcher (pas d'aide non plus)

--- Script.js
+++ Script.js
@@ -1,57 +1,148 @@
+// Script.js récupéré de la correction car ni moi ni le prof n'arrivait à faire marcher ma solution au dernier cours
+
 function reduireArray(array, size) {
-	if (array.length <= size) {
-		return [array];
-	}
-	return [array.slice(0, size),
-	...reduireArray(array.slice(size), size)];
+  if (array.length <= size) {
+    return [array];
+  }
+  return [array.slice(0, size), ...reduireArray(array.slice(size), size)];
 }
 
 const dateTimeFormat = Intl.DateTimeFormat("fr");
 
 function afficher(json){
 	const selections = reduireArray(json, 3);
-	let html = "";
-	selections.forEach(selection => {
-		html += '<div class="columns">';
-		selection.forEach(repo => {
-			html += `
-			<div class="column">
-				<div class="card">
-					<div class="card-image">
-						<figure class="image is-4by3">
-							<img src="${repo.image}" alt="Placeholder image"/>
-						</figure>
-					</div>
-					<div class="card-content">
-						<div class="media">
-							<div class="media-left">
-								<figure class="image is-48x48">
-									<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-								</figure>
-							</div>
-							<div class="media-content">
-								<p class="title is4">${repo.name}</p>
-								<p class="subtitle is6">@Parcourir</p>
-							</div>
-						</div>
 
-						<div class="content">
-							${repo.description}
-							<br />
-							Dernière mise à jour: <time datetime="${repo.updated_at}"> ${dateTimeFormat.format(new Date(repo.updated_at))}</time>
-						</div>
-					</div>
-				</div>
-			</div>`;
-		});
-		html += "</div>";
-	});
-	document.querySelector(".container").innerHTML =
-	html;
+  let html = "";
+
+  selections.forEach(selection => {
+    html += '<div class="columns">';
+
+    selection.forEach((repo) => {
+      html += `
+            <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">${repo.name}</p>
+                    <p class="subtitle is-6">@Parcourir</p>
+                  </div>
+                </div>
+  
+                <div class="content">
+                   ${repo.description}
+                  <br />
+                  Dernière mise à jour: <time datetime="${
+                    repo.updated_at
+                  }">${dateTimeFormat.format(new Date(repo.updated_at))}</time>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    });
+    html += "</div>";
+  });
+
+  document.querySelector(".container").innerHTML = html;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-	fetch("/GalerieRepos/tableau.json")
-		.then(response => response.json())
-		.then(json => afficher(json));
+document.addEventListener("DOMContentLoaded", function () {
+  if (navigator.onLine) {
+    document.querySelector(".notification").setAttribute("hidden", "");
+  }
+
+  window.addEventListener("online", () => {
+    document.querySelector(".notification").setAttribute("hidden", "");
+  });
+  window.addEventListener("offline", () => {
+    document.querySelector(".notification").removeAttribute("hidden");
+  });
+// Modification par rapport au script.js de la correction : changer l'url à fetch
+  let fetchData;
+  if (navigator.onLine) {
+    fetchData = fetch("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json")
+      .then((response) => response.json())
+      .then((data) => localforage.setItem("data", data));
+  } 
+  else {
+    fetchData = localforage.getItem("data");
+  }
+
+  fetchData.then((json) => afficher(json));
 });
+
+// Ancien Script.js
+
+// function reduireArray(array, size) {
+	// if (array.length <= size) {
+		// return [array];
+	// }
+	// return [array.slice(0, size),
+	// ...reduireArray(array.slice(size), size)];
+// }
+
+// const dateTimeFormat = Intl.DateTimeFormat("fr");
+
+// function afficher(json){
+	// const selections = reduireArray(json, 3);
+	// let html = "";
+	// selections.forEach(selection => {
+		// html += '<div class="columns">';
+		// selection.forEach(repo => {
+			// html += `
+			// <div class="column">
+				// <div class="card">
+					// <div class="card-image">
+						// <figure class="image is-4by3">
+							// <img src="${repo.image}" alt="Placeholder image"/>
+						// </figure>
+					// </div>
+					// <div class="card-content">
+						// <div class="media">
+							// <div class="media-left">
+								// <figure class="image is-48x48">
+									// <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+								// </figure>
+							// </div>
+							// <div class="media-content">
+								// <p class="title is4">${repo.name}</p>
+								// <p class="subtitle is6">@Parcourir</p>
+							// </div>
+						// </div>
+
+						// <div class="content">
+							// ${repo.description}
+							// <br />
+							// Dernière mise à jour: <time datetime="${repo.updated_at}"> ${dateTimeFormat.format(new Date(repo.updated_at))}</time>
+						// </div>
+					// </div>
+				// </div>
+			// </div>`;
+		// });
+		// html += "</div>";
+	// });
+	// document.querySelector(".container").innerHTML =
+	// html;
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+	// fetch("/GalerieRepos/tableau.json")
+		// .then(response => response.json())
+		// .then(json => afficher(json));
+// });
--- sw.js
+++ sw.js
@@ -1,73 +1,148 @@
-// Test
-// console.log("test s'il est interprété OK!");
-// Fin Test
+// Correction récupérée car ni moi ni le prof n'avons su faire marcher mon service worker au dernier cours
+const cacheName = "galerie";
 
-// Test si le service worker reconnait les messages (type d'événement = 'message') envoyés
-// self.addEventListener('message',event=>
-// {
-    // this.clients.matchAll()
-    // .then(clients=>
-    // {
-        // clients.forEach(
-            // client=>client.postMessage('Enchanté, je suis le service worker')
-        // );
-    // });
-// });
+const files = [
+  "/",
+  "/Script.js",
+  "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css",
+  "https://bulma.io/images/placeholders/1280x960.png",
+  "https://bulma.io/images/placeholders/96x96.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"
+];
 
-// Ecoute de tous les évènements fetch et récupération de chaque URL
-//self.addEventListener("fetch", event => {console.log(event.request.url);});
-
-// Ecoute de tous les évènements fetch et récupération de l’URL permettant l’obtention de images.jsonpuis reformatage de images.json
-self.addEventListener("fetch", (event) => 
-{
-	// Test
-	console.log("On a trouvé un fetch");
-	console.log("L'url.indexOf c'est ça : " + url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json"));
-	// Fin Test
-	
-	const url = event.request.url;
-	
-	// Test
-	console.log("L'url c'est ça : " + url);
-	// Fin Test
-	
-	if (url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json") ===0) 
-	{
-		console.log("Le fetch est sur une requête vers notre tableau.json");
-		event.respondWith(fetch(event.request)
-		.then((response) => 
-		{
-			console.log(response.status);
-			if (response.status === 200) 
-			{
-				console.info("Formatting data");
-				return response.json().then((json) => 
-				{
-					const formattedResponse = json.map((j) => (
-					{
-						name: j.name,
-						description: j.description || "",
-						updated_at: j.updated_at,
-						created_at: j.created_at,
-						image: j.image
-					}));
-					return new Response(JSON.stringify(formattedResponse));
-				});
-				
-			}else{
-				console.error(
-					"Service Worker",
-					"Error when fetching",
-					event.request.url
-				);
-				return response;
-			}
-		}));
-	}
+self.addEventListener("install", e => {
+  caches.open(cacheName).then(cache => {
+    cache.addAll(files);
+  });
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
-// Si on reconnait un fetch, on écrit "PWA!!!!"
-// self.addEventListener('fetch',event=>
-// {    
-    // console.log(newResponse('PWA!!!!'));
+self.addEventListener("fetch", e => {console.log(e.request.url);});
+
+self.addEventListener("fetch", event => {
+  const url = event.request.url;
+
+  if (url.indexOf("https://nostalgic-lamarr-5a666c.netlify.app/images.json") === 0) {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        if (response.status === 200) {
+			console.info("Formatting data");
+			return response.json().then(json => {
+			const formattedResponse = json.map(j => ({
+			name: j.name,
+			description: j.description || "",
+			updated_at: j.updated_at
+			}));
+
+			return new Response(JSON.stringify(formattedResponse));
+			});
+        }
+		else{
+			console.error(
+			"Service Worker",
+			"Error when fetching",			
+			event.request.url
+			);
+
+			return response;
+		}
+        
+      })
+    );
+  } 
+  else {
+    event.respondWith(
+      caches
+        .open(cacheName)
+        .then(cache => cache.match(event.request))
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
+
+
+// // Test
+// // console.log("test s'il est interprété OK!");
+// // Fin Test
+
+// // Test si le service worker reconnait les messages (type d'événement = 'message') envoyés
+// // self.addEventListener('message',event=>
+// // {
+    // // this.clients.matchAll()
+    // // .then(clients=>
+    // // {
+        // // clients.forEach(
+            // // client=>client.postMessage('Enchanté, je suis le service worker')
+        // // );
+    // // });
+// // });
+
+// // Ecoute de tous les évènements fetch et récupération de chaque URL
+// //self.addEventListener("fetch", event => {console.log(event.request.url);});
+
+// // Ecoute de tous les évènements fetch et récupération de l’URL permettant l’obtention de images.jsonpuis reformatage de images.json
+// self.addEventListener("fetch", (event) => 
+// {
+	// // Test
+	// console.log("On a trouvé un fetch");
+	// console.log("L'url.indexOf c'est ça : " + url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json"));
+	// // Fin Test
+	
+	// const url = event.request.url;
+	
+	// // Test
+	// console.log("L'url c'est ça : " + url);
+	// // Fin Test
+	
+	// if (url.indexOf("https://trusting-swirles-3a325e.netlify.app/GalerieRepos/tableau.json") ===0) 
+	// {
+		// console.log("Le fetch est sur une requête vers notre tableau.json");
+		// event.respondWith(fetch(event.request)
+		// .then((response) => 
+		// {
+			// console.log(response.status);
+			// if (response.status === 200) 
+			// {
+				// console.info("Formatting data");
+				// return response.json().then((json) => 
+				// {
+					// const formattedResponse = json.map((j) => (
+					// {
+						// name: j.name,
+						// description: j.description || "",
+						// updated_at: j.updated_at,
+						// created_at: j.created_at,
+						// image: j.image
+					// }));
+					// return new Response(JSON.stringify(formattedResponse));
+				// });
+				
+			// }else{
+				// console.error(
+					// "Service Worker",
+					// "Error when fetching",
+					// event.request.url
+				// );
+				// return response;
+			// }
+		// }));
+	// }
 // });
+
+// // Si on reconnait un fetch, on écrit "PWA!!!!"
+// // self.addEventListener('fetch',event=>
+// // {    
+    // // console.log(newResponse('PWA!!!!'));
+// // });
